cb631e871b702ddae0f4820264d0ce6079c382d9
Daniil Muravetskii <mdr333@mail.ru>
2024-07-25 12:34:23 +0400

    Update TransformerModelLooped_N_LastToken.py

--- scripts/TransformerModelLooped_N_LastToken.py
+++ scripts/TransformerModelLooped_N_LastToken.py
@@ -1,3 +1,81 @@
+class TransformerModel(nn.Module):
+    def __init__(self, n_dims, n_positions, n_embd=128, n_layer=12, n_head=4, pred_type='regression'):
+
+        super(TransformerModel, self).__init__()
+        self.freq = 2
+        self.ind = 0
+        configuration = GPT2Config()
+        configuration.block_size = self.freq * n_positions + 1
+        configuration.n_layer = n_layer
+        configuration.n_head = n_head
+        configuration.n_embd = n_embd
+        configuration.dropout = 0.0
+        configuration.bias = True
+        configuration.dropout = 0.
+        self.configuration = configuration
+
+        self.n_positions = n_positions  # n = points in this setting
+        self.n_dims = n_dims  # input dimension, d_in
+        self.n_embd = n_embd  # d
+        self.n_layer = n_layer
+        self._pred_type = pred_type
+
+        self._read_in = nn.Linear(n_dims, n_embd)
+        self._backbone = GPT2Model(self.configuration)
+        if self._pred_type == 'regression':
+            self._read_out = nn.Linear(n_embd, 1)
+        elif self._pred_type == 'classification':
+            self._read_out = nn.Linear(n_embd, MAX_NUM_CLASS)  # NOTE: hard-code
+
+        self.print_flag = False
+
+    def _combine(self, xs_b, ys_b):
+        """
+        :param xs_b: shape [B, n, d_in]
+        :param ys_b: shape [B, n]
+        :return: shape [B, 2n, d_in + 1]
+        """
+        B, n, d = xs_b.shape
+        device = xs_b.device
+
+        ys_b_wide = torch.cat(
+            (
+                ys_b.view(B, n, 1),
+                torch.zeros(B, n, d-1, device=device),
+            ),
+            axis=2,
+        )
+
+        zs = torch.stack((xs_b, ys_b_wide), dim=2)
+        zs = zs.view(B, self.freq * n, d)
+
+        return zs
+
+    def forward(self, xs, ys, add_inputs_embeds=False):
+        """
+        :param xs: [B, n, d]
+        :param ys: [B, n]
+        :return:
+        """
+
+        B, n, d_in = xs.shape
+        zs = self._combine(xs, ys)  # [B, n, d_in], [B, n], [B, n] -> [B, 2n, d_in + 1]
+        embeds = self._read_in(zs)  # [B, 2n, d_in + 1] -> [B, 2n, d]
+
+        f_output = self._backbone(
+            inputs_embeds=embeds, position_ids=None, rm_pos_embd=False, add_inputs_embeds=add_inputs_embeds)  # [B, 2n, d]
+        prediction = self._read_out(f_output)  # [B, 2n, d] -> [B, 2n, 1]
+        if self._pred_type == 'regression':
+            y = prediction[:, self.ind::self.freq, 0]
+        elif self._pred_type == 'classification':
+            y = prediction[:, self.ind::self.freq]
+        else:
+            raise NotImplementedError
+
+        return y
+
+
+
 class TransformerModelLooped_N_LastTokens(TransformerModel):
     def __init__(
             self, n_dims, n_positions, n_embd=128, n_layer=12, n_head=4, loop_func='z=f(x+z)', pred_type='regression'):
