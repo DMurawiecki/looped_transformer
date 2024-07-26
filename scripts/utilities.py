f268cdfb481865172b7fd038d36253cd3f4a49c4
Daniil Muravetskii <mdr333@mail.ru>
2024-07-27 00:30:15 +0400

    Update utilities.py

--- scripts/utilities.py
+++ scripts/utilities.py
@@ -6,7 +6,9 @@ def set_optim_scaler_curriculum_by_args(args_path, model):
   with open(args_path, 'r') as config_file:
     args = yaml.safe_load(config_file)
   dtype = 'float16'  # 'bfloat16', 'float32'
-  optimizer = torch.optim.Adam(model.parameters(), lr=args['training']['learning_rate'], weight_decay=args['training']['weight_decay'])
+  optimizer = torch.optim.Adam(model.parameters(), 
+                               lr=args['training']['learning_rate'], 
+                               weight_decay=args['training']['weight_decay'])
   scaler = torch.cuda.amp.GradScaler(enabled=(dtype == 'float16'))
   curriculum = Curriculum(args['training']['curriculum'])
-  return optimizer, scaler, curriculum
+  return optimizer, scaler, curriculum, args
