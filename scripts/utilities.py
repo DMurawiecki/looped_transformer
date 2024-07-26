4e20d9ef779ffbd4293f3359daa52369367f1a69
Daniil Muravetskii <mdr333@mail.ru>
2024-07-27 00:18:45 +0400

    Add files via upload

---
+++ scripts/utilities.py
@@ -0,0 +1,8 @@
+def set_optim_scaler_curriculum_by_args(args_path):
+  with open(args_path, 'r') as config_file:
+    args = yaml.safe_load(config_file)
+
+  optimizer = torch.optim.Adam(model.parameters(), lr=args['training']['learning_rate'], weight_decay=args['training']['weight_decay'])
+  scaler = torch.cuda.amp.GradScaler(enabled=(dtype == 'float16'))
+  curriculum = Curriculum(args['training']['curriculum'])
+  return optimizer, scaler, curriculum
