594d5fba0abb17ba68e04d15277c68cceea109e7
Daniil Muravetskii <mdr333@mail.ru>
2024-07-31 16:20:34 +0400

    Update nano_gpt.py

--- scripts/nano_gpt.py
+++ scripts/nano_gpt.py
@@ -134,7 +134,7 @@ class SSM_Block(nn.Module):
         self.mlp = MLP(config)
 
     def forward(self, x):
-        x = x + self.attn(self.ln_1(x))
+        x = x + self.ssm(self.ln_1(x))
         x = x + self.mlp(self.ln_2(x))
         return x
 
