142e10facc1b95b04aca928eff494831ea67265b
Daniil Muravetskii <mdr333@mail.ru>
2024-07-31 15:54:22 +0400

    Update nano_gpt.py

--- scripts/nano_gpt.py
+++ scripts/nano_gpt.py
@@ -16,6 +16,7 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from zeta.nn import SSM
 
 
 # @torch.jit.script # good to enable when not using torch.compile, disable when using (our default)
@@ -122,6 +123,22 @@ class Block(nn.Module):
         return x
 
 
+class SSM_Block(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = LayerNorm(config.n_embd, bias=config.bias)
+        # self.attn = CausalSelfAttention(config)
+        self.ssm = SSM(in_features=config.n_embd, dt_rank=16, dim_inner=32, d_state=64)
+        self.ln_2 = LayerNorm(config.n_embd, bias=config.bias)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
 @dataclass
 class GPT2Config:
     block_size: int = 1024
@@ -145,6 +162,83 @@ class GPT2Model(nn.Module):
             wpe=nn.Embedding(config.block_size, config.n_embd),
             drop=nn.Dropout(config.dropout),
             h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f=LayerNorm(config.n_embd, bias=config.bias),
+        ))
+
+        # init all weights
+        self.apply(self._init_weights)
+        # apply special scaled init to the residual projections, per GPT-2 paper
+        for pn, p in self.named_parameters():
+            if pn.endswith('c_proj.weight'):
+                torch.nn.init.normal_(p, mean=0.0, std=0.02 / math.sqrt(2 * config.n_layer))
+
+        # report number of parameters
+        print("number of parameters: %.2fM" % (self.get_num_params() / 1e6,))
+
+    def get_num_params(self, non_embedding=True):
+        """
+        Return the number of parameters in the model.
+        For non-embedding count (default), the position embeddings get subtracted.
+        The token embeddings would too, except due to the parameter sharing these
+        params are actually used as weights in the final layer, so we include them.
+        """
+        n_params = sum(p.numel() for p in self.parameters())
+        if non_embedding:
+            n_params -= self.transformer.wpe.weight.numel()
+        return n_params
+
+    def _init_weights(self, module):
+        # this is the same as huggingface repo:
+        # initialize range: https://huggingface.co/transformers/v3.0.2/_modules/transformers/configuration_gpt2.html#GPT2Config
+        # initialize function: https://huggingface.co/transformers/v3.0.2/_modules/transformers/modeling_gpt2.html
+        # search there _init_weights function
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, inputs_embeds, position_ids=None, rm_pos_embd=False, add_inputs_embeds=False, output_intermediate=False):
+        device = inputs_embeds.device
+        b, t, d = inputs_embeds.size()
+        assert t <= self.config.block_size, f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
+        if position_ids is None:
+           position_ids = torch.arange(0, t, dtype=torch.long, device=device).unsqueeze(0)  # shape (1, t)
+        if output_intermediate:
+            embeds = [inputs_embeds]
+
+        # forward the GPT model itself
+        # tok_emb = self.transformer.wte(idx)  # token embeddings of shape (b, t, n_embd)
+        pos_emb = self.transformer.wpe(position_ids)  # position embeddings of shape (1, t, n_embd)
+        if rm_pos_embd:
+            pos_emb = torch.zeros_like(pos_emb, device=device)
+        x = self.transformer.drop(inputs_embeds + pos_emb)
+        for block in self.transformer.h:
+            if add_inputs_embeds:
+                x = block(x + inputs_embeds)
+            else:
+                x = block(x)
+            if output_intermediate:
+                embeds.append(x)
+        x = self.transformer.ln_f(x)
+        if output_intermediate:
+            return x, embeds
+
+        return x
+
+class SSM_GPT2Model(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        assert config.vocab_size is not None
+        assert config.block_size is not None
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            wpe=nn.Embedding(config.block_size, config.n_embd),
+            drop=nn.Dropout(config.dropout),
+            h=nn.ModuleList([SSM_Block(config) for _ in range(config.n_layer)]),
             ln_f=LayerNorm(config.n_embd, bias=config.bias),
         ))
 
