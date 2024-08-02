cc688e9bee05ce462954601525e76eb8c99e22c5
Daniil Muravetskii <mdr333@mail.ru>
2024-08-02 13:42:58 +0400

    Update train_implementation.py

--- scripts/train_implementation.py
+++ scripts/train_implementation.py
@@ -129,7 +129,7 @@ def train_model_new(starting_step, ending_step, args, model, ctx, add_inputs_emb
                                                           scaler= scaler,
                                                           add_inputs_embeds= add_inputs_embeds)
     losses_train.append(loss)
-    val_loss = evaluate_model(model= model, args= args, curriculum=curriculum)
+    val_loss = evaluate_model(model= model, args= args, curriculum=curriculum, device=device)
     losses_val.append(val_loss)
     wandb.log({"training loss": losses_train, 
                "validation loss": losses_val},
