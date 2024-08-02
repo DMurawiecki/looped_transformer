1e5ba6b72f0227b4c4ab1ad70bf5dc9396d70493
Daniil Muravetskii <mdr333@mail.ru>
2024-08-02 13:02:25 +0400

    Update train_implementation.py

--- scripts/train_implementation.py
+++ scripts/train_implementation.py
@@ -2,7 +2,8 @@ import torch
 from tasks import get_task_sampler
 from tqdm import tqdm
 from train import calculate_gradient_norm
-
+from eval_and_save import evaluate_model
+import wandb
 
 def train_step(args, curriculum, model, xs, ys, optimizer, ctx, scaler, add_inputs_embeds):
     if args['model']['family'] in ['gpt2', 'gpt2_tying']:
@@ -84,3 +85,57 @@ def train_model(starting_step, ending_step, args, model, ctx, add_inputs_embeds,
     losses.append(loss)
 
   return losses
+
+def train_model_new(starting_step, ending_step, args, model, ctx, add_inputs_embeds, optimizer, curriculum, scaler, device, run_name):
+  torch.set_float32_matmul_precision('highest')
+  torch.backends.cuda.matmul.allow_tf32 = True  # allow tf32 on matmul
+  torch.backends.cudnn.allow_tf32 = True  # allow tf32 on cudnn
+  dtype = 'float16'  # 'bfloat16', 'float32'
+  ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]  
+  if ctx:
+    ctx = torch.amp.autocast(device_type='cuda', dtype=ptdtype, cache_enabled=False)
+
+  model.train()
+  pbar = tqdm(range(starting_step, ending_step))
+  losses_train = []
+  losses_val = []
+  wandb.init(
+    # Set the project where this run will be logged
+    project= run_name
+    # Track hyperparameters and run metadata
+    config={
+        "learning_rate": args['training']['learning_rate'],
+        "epochs": ending_step - strating_step,
+    },
+)
+  for i in pbar:
+    task_sampler = get_task_sampler(
+                    task_name=args['training']['task_name'],
+                    batch_size=args['training']['batch_size'],
+                    n_points=curriculum.n_points,
+                    n_dims=args['model']['n_dims'],
+                    n_dims_truncated=curriculum.n_dims_truncated,
+                    device=device)
+
+    real_task = task_sampler()
+    xs, ys = real_task.xs.float(), real_task.ys.float()
+
+
+    loss, output, total_norm, grad_norm_dict = train_step(args= args,
+                                                          curriculum= curriculum,
+                                                          model= model,
+                                                          xs = xs,
+                                                          ys = ys,
+                                                          optimizer=optimizer,
+                                                          ctx= ctx,
+                                                          scaler= scaler,
+                                                          add_inputs_embeds= add_inputs_embeds)
+    losses_train.append(loss)
+    val_loss = evaluate_model(model= model, args= args, curriculum=cu)
+    losses_val.append(val_loss)
+    wandb.log({"training loss": losses_train, 
+               "validation loss": losses_val},
+               step = i)
+
+  return losses_train, losses_val
+
